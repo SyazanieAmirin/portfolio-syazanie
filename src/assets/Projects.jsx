c5c33465555bce7d8f56053555da0a5a99eb8714
Syazanie <syazamirin@outlook.com>
2024-09-21 19:30:14 +0800

    v1.0.1 + Updated the too dark colour

--- src/assets/Projects.jsx
+++ src/assets/Projects.jsx
@@ -4,7 +4,7 @@ import Button from "../assets/components/ButtonCOMP"
 export default function Projects() {
     return (
         <main className="flex flex-col gap-5 mt-5 lg:flex-row flex-wrap animate-slide-right overflow-x-hidden">
-            <div className="flex flex-col gap-5 bg-[#0E0E0E] w-full md:w-auto rounded-lg lg:max-w-[350px]">
+            <div className="flex flex-col gap-5 bg-[#272727] w-full md:w-auto rounded-lg lg:max-w-[350px]">
                 <img src={PetaMetroThumbnail} className="rounded-t-lg lg:max-w-[350px] h-full w-full" />
                 <div className="flex flex-col gap-3 px-3 pb-3">
                     <h2 className="text-xl font-bold">Peta Metro</h2>
